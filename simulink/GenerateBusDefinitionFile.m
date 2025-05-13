d75de00cce51505379b640b0770dd4e3f9652ced
PeterC <petercalifano.gs@gmail.com>
2025-05-13 23:33:49 +0200

    Add prototype of bus definition generator and unit test

--- simulink/GenerateBusDefinitionFile.m
+++ simulink/GenerateBusDefinitionFile.m
@@ -1,63 +1,175 @@
-function [cellBuses] = GenerateBusDefinitionFile(charDefFilename, cellDataStructs)
+function GenerateBusDefinitionFile(strInput, charBusName, charOutputFolder, kwargs)
 arguments
-    charDefFilename (:,1) string
-    cellDataStructs (:,1) cell = {}
+    strInput
+    charBusName
+    charOutputFolder
+end
+arguments
+    kwargs.bStoreExampleValues {islogical, isscalar} = false
+end
+%% DESCRIPTION
+% What the function does
+% -------------------------------------------------------------------------------------------------------------
+%% INPUT
+% in1 [dim] description
+% Name1                     []
+% Name2                     []
+% Name3                     []
+% -------------------------------------------------------------------------------------------------------------
+%% OUTPUT
+% out1 [dim] description
+% Name1                     []
+% Name2                     []
+% Name3                     []
+% -------------------------------------------------------------------------------------------------------------
+%% CHANGELOG
+% 13-05-2025    Pietro Califano & o4-mini-high      First prototype version.
+% -------------------------------------------------------------------------------------------------------------
+%% DEPENDENCIES
+% [-]
+% -------------------------------------------------------------------------------------------------------------
+%% Future upgrades
+% [-]
+% -------------------------------------------------------------------------------------------------------------
+%% Function code
+
+% Create output folder if needed
+if ~exist(charOutputFolder, 'dir')
+    mkdir(charOutputFolder);
 end
 
-if isempty(cellDataStructs)
-    disp('Input data is an empty cell. No file will be generated.')
-    cellBuses = {};
-    return;
+% Track which buses have been generated
+mapVisited = containers.Map();
+WriteBusFile(strInput, charBusName, charOutputFolder, mapVisited, kwargs);
 end
 
-% Open file to write 
-% file = open(charDefFilename, 'w');
-
-% Get number of buses to define
-ui32NumOfBuses = size(cellDataStructs, 1);
-
-for idInst = 1:ui32NumOfBuses
-
-    % GenerateBusDefinition(cellDataStructs{idInst});
+%%% Core function: bus definition code-generator
+function WriteBusFile(varStruct, charBusName, charOutputFolder, objDefinedFieldsMap, kwargs)
+% Avoid regenerating the same bus
+if objDefinedFieldsMap.isKey(charBusName)
+    return
 end
 
-%% LOCAL function
-    function [charBusDefinition] = GenerateBusDefinition(busInput)
-        % arguments
-        %     strBusInput (1,1) struct {isscalar}
-        % end
+objDefinedFieldsMap(charBusName) = true;
 
-        % TODO: check if any of the fields is a struct
-        % If yes --> perform recursive calls
+% Alphabetically sorted fields
+cellFieldNames = fieldnames(orderfields(varStruct));
 
-        if isa(busInput, 'struct')
-            % Generate Simulink bus object from struct
-            objSimulinkBus; 
-            
-        elseif isa(busInput, 'Simulink.Bus')
-            objSimulinkBus = busInput;
+% Open definition file
+charFilePath = fullfile(charOutputFolder, strcat('Define_', charBusName, '.m'));
+i32Fid = fopen(charFilePath, 'w');
+if i32Fid == -1
+    error("Cannot open '%s' for writing.", charFilePath)
+end
+
+% Write function signature
+fprintf(i32Fid, 'function [Bus');
+if kwargs.bStoreExampleValues
+    fprintf(i32Fid, ', Example');
+end
+fprintf(i32Fid, '] = Define_%s()\n', charBusName);
+% Write file header
+fprintf(i32Fid, '%% Auto-generated Simulink.Bus definition for %s\n\n', charBusName); 
+% TODO add details such as data, author and system
+
+% TODO: add error if field is a cell or something else (not supported)
+
+%%% Define nested buses (struct) by recursion
+for ui32Idx = 1:numel(cellFieldNames)
+
+    charField = cellFieldNames{ui32Idx};
+    varVal    = varStruct.(charField);
+
+    if isstruct(varVal) || isobject(varVal)
+
+        charSubBus = sprintf('%s_%s', charBusName, charField);
+
+        % Recurse to generate sub-bus
+        WriteBusFile(varVal, charSubBus, charOutputFolder, objDefinedFieldsMap, kwargs);
+        
+        % Write call to bus definition % TODO add an eval to move bus definition to workspace of "top
+        % caller"
+        if kwargs.bStoreExampleValues
+            fprintf(i32Fid, '[Bus_%s, Example_%s] = Define_%s();\n', ...
+                charSubBus, charSubBus, charSubBus);
+            fprintf(i32Fid, 'evalin("base", "Bus_%s");\n', charSubBus);
+            fprintf(i32Fid, 'evalin("base", "Example_%s");\n', charSubBus);
         else
-            error('Invalid input object: must be a struct or a Simulink.Bus!')
+            fprintf(i32Fid, 'Bus_%s = Define_%s();\n', charSubBus, charSubBus);
+            fprintf(i32Fid, 'evalin("base", "Bus_%s");', charSubBus);
         end
-
-        % Get fields of bus and data to write
-        % Example: 
-        % Bus with properties:
-        %
-        %                 Description: ''
-        %                   DataScope: 'Auto'
-        %                  HeaderFile: ''
-        %                   Alignment: -1
-        %   PreserveElementDimensions: 0
-        %                    Elements: [0×0 Simulink.BusElement]
-
-        cellFields = fieldnames(objSimulinkBus);
-        % cellData;
-
-        % Print definition to char array
-        % TODO
 
     end
 
 end
+fprintf(i32Fid, '\n');
 
+%% Build the BusElement list
+fprintf(i32Fid, 'elems = Simulink.BusElement.empty;\n\n');
+for ui32Idx = 1:numel(cellFieldNames)
+    charField = cellFieldNames{ui32Idx};
+    varVal    = varStruct.(charField);
+
+    fprintf(i32Fid, 'elem = Simulink.BusElement;\n');
+    fprintf(i32Fid, 'elem.Name = ''%s'';\n', charField);
+
+    if isstruct(varVal) || isobject(varVal)
+        charSubBus = sprintf('%s_%s', charBusName, charField);
+        fprintf(i32Fid, 'elem.DataType = ''Bus: %s'';\n', charSubBus);
+        fprintf(i32Fid, 'elem.Dimensions = 1;\n');
+    else
+        dDims = size(varVal);
+        if ischar(varVal)
+            dDims = [1, numel(varVal)];
+        end
+        fprintf(i32Fid, 'elem.DataType = ''%s'';\n', class(varVal));
+        fprintf(i32Fid, 'elem.Dimensions = %s;\n', mat2str(dDims));
+    end
+
+    fprintf(i32Fid, 'elems(end+1) = elem;\n\n');
+end
+
+%% Create the bus object
+fprintf(i32Fid, 'Bus = Simulink.Bus;\n');
+fprintf(i32Fid, 'Bus.Elements = elems;\n\n');
+
+%% Optional example struct
+if kwargs.bStoreExampleValues
+    fprintf(i32Fid, '%% Example struct initialization\n');
+    fprintf(i32Fid, 'Example = struct();\n');
+    for ui32Idx = 1:numel(cellFieldNames)
+        charField = cellFieldNames{ui32Idx};
+        varVal    = varStruct.(charField);
+
+        if isstruct(varVal) || isobject(varVal)
+            charSubBus = sprintf('%s_%s', charBusName, charField);
+            fprintf(i32Fid, 'Example.%s = Example_%s;\n', charField, charSubBus);
+        else
+            charValStr = FormatValue(varVal);
+            fprintf(i32Fid, 'Example.%s = %s;\n', charField, charValStr);
+        end
+    end
+    fprintf(i32Fid, '\n');
+end
+
+fprintf(i32Fid, 'end\n');
+fclose(i32Fid);
+
+fprintf('Generated: %s\n', charFilePath);
+
+
+end
+
+%% Auxiliary functions
+% Value formatting function
+function charVal = FormatValue(varVal)
+if ischar(varVal)
+    charVal = ['''' varVal ''''];
+elseif islogical(varVal) || (~isempty(varVal) && isnumeric(varVal))
+    charVal = mat2str(varVal);
+else
+    charVal = '[]';
+end
+end
+
+% Bus loading function
--- tests/testAutomaticBusDefinition.m
+++ tests/testAutomaticBusDefinition.m
@@ -3,4 +3,27 @@ clear
 clc
 
 % TEST SETUP
+strTestStruct.bFlag = true;
+strTestStruct.dSigmaValue = true;
+strTestStruct.strAttitudeData.ui8PolyDeg = uint8(8);
+strTestStruct.strAttitudeData.dPolyCoeff = zeros(4,10);
+strTestStruct.ui32MaxNumOfFrames = uint32(50);
+
+
+%% test_GenerateBusDefinitionFile
+charOutputFolder    = ".";
+charBusName         = "testBus";
+strInput            = strTestStruct;
+
+GenerateBusDefinitionFile(strInput, charBusName, charOutputFolder);
+
+%% test_GenerateBusDefinitionFile_withDefaults
+charOutputFolder    = ".";
+charBusName         = "testBusWithExample";
+strInput            = strTestStruct;
+
+GenerateBusDefinitionFile(strInput, ...
+    charBusName, ...
+    charOutputFolder, ...
+    "bStoreExampleValues", true);
 
