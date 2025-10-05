4359ee5ae03ba076f40090aaba797e580ea227fb
PeterC <petercalifano.gs@gmail.com>
2025-10-05 19:19:26 +0200

    Add test for struct arrays handling (WIP)

--- tests/testAutomaticBusDefinition.m
+++ tests/testAutomaticBusDefinition.m
@@ -2,6 +2,10 @@ close all
 clear
 clc
 
+cd( fileparts(mfilename("fullpath")) );
+addpath('..');
+SetupPaths_AutoCodegenTools4MATLAB;
+
 % TEST SETUP
 strTestStruct.bFlag = true;
 strTestStruct.dSigmaValue = 10;
@@ -9,13 +13,44 @@ strTestStruct.strAttitudeData.ui8PolyDeg = uint8(8);
 strTestStruct.strAttitudeData.dPolyCoeff = zeros(4,10);
 strTestStruct.ui32MaxNumOfFrames = uint32(50);
 
-%% test_GenerateBusDefinitionFile
+strStructArray = struct();
+strStructArray(1).dRandomVar = randn(3,1);
+strStructArray(2).dRandomVar = randn(3,1);
+strStructArray(3).dRandomVar = randn(3,1);
+strStructArray(3).dAdditionalVarToTryToBreak = 0.0;
+
+strTestStructWithStructArray = struct();
+strTestStructWithStructArray.strStructArray = strStructArray;
+strTestStructWithStructArray.bAnotherBool = true;
+return
+
+%% test_GenerateBusDefinitionFile_basic
 charOutputFolder    = "./bus_autodefs";
 charBusName         = "testBus";
-strInput            = strTestStruct;
 
-GenerateBusDefinitionFile(strInput, charBusName, charOutputFolder);
+GenerateBusDefinitionFile(strTestStruct, ...
+                        charBusName, ...
+                        charOutputFolder, ...
+                        "bCleanupBeforeGeneration", true, ...
+                        "bDefineBusesInPlace", true);
 
+% Call bus definition to check usage
+run(sprintf('%s/BusDef_%s.m', charOutputFolder, charBusName));
+
+return
+%% test_GenerateBusDefinitionFile_withStructArray
+charOutputFolder    = "./bus_autodefs_struct_array";
+charBusName         = "testBusWithStructArray";
+
+GenerateBusDefinitionFile(strTestStructWithStructArray, ...
+                        charBusName, ...
+                        charOutputFolder, ...
+                        "bCleanupBeforeGenerati on", true, ...
+                        "bDefineBusesInPlace", true);
+
+run(sprintf('%s/BusDef_%s.m', charOutputFolder, charBusName));
+
+return
 %% test_GenerateBusDefinitionFile_withDefaults
 charOutputFolder    = "./bus_autodefs";
 charBusName         = "testBusWithExample";
@@ -32,4 +67,4 @@ GenerateBusDefinitionFile(strInput, ...
 strInput.strAttitudeData = orderfields(strInput.strAttitudeData); % Order fields of nested struct
 compareStructures(testBusWithExample, orderfields(strInput));
 
-
+return
